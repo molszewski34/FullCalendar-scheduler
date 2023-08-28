077cfca2dd110ae8f992db22284fe11fb0159b87
molszewski34 <m.olszewski341@gmail.com>
2023-08-28 08:58:30 +0200

    delete event feature

--- client/src/Components/Calendar.jsx
+++ client/src/Components/Calendar.jsx
@@ -1,17 +1,20 @@
 import React, { useRef, useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction'; // Dodane
+import interactionPlugin from '@fullcalendar/interaction';
 import AddEventModal from './AddEventModal';
+import Datetime from 'react-datetime';
 import axios from 'axios';
 import moment from 'moment';
+
 const Calendar = () => {
   const [modalOpen, setModalOpen] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(null); // Dodane
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedEvent, setSelectedEvent] = useState(null);
   const calendarRef = useRef(null);
-  const [events, setEvents] = useState('');
-
-  console.log(events);
+  const [events, setEvents] = useState([]);
+  const [overlay, setOverlay] = useState(false);
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
 
   const onEventAdded = (event) => {
     let calendarApi = calendarRef.current.getApi();
@@ -22,50 +25,107 @@ const Calendar = () => {
     });
   };
 
+  console.log(selectedEvent);
+
   const handleDateClick = (arg) => {
     setSelectedDate(arg.date);
     setModalOpen(true);
   };
 
-  async function handleEventAdd(data) {
+  const handleEventAdd = async (data) => {
     await axios.post('/api/calendar/create-event', data.event);
-  }
+  };
 
-  async function handleDateSet(data) {
+  const handleDateSet = async (data) => {
     const response = await axios.get(
       '/api/calendar/get-events?start=' +
         moment(data.start).toISOString() +
         '&end=' +
         moment(data.end).toISOString()
+      // data.start.format('YYYY-MM-DD') +
+      // '&end=' +
+      // data.end.format('YYYY-MM-DD')
     );
     setEvents(response.data);
-  }
+  };
+
+  const handleEventDelete = async () => {
+    if (selectedEvent) {
+      await axios.delete(
+        `/api/calendar/delete-event/${selectedEvent._def.extendedProps._id}`
+      );
+      setDeleteConfirmationOpen(false);
+      setSelectedEvent(null);
+    }
+  };
 
   return (
     <section>
-      <button onClick={() => setModalOpen(true)}>Add Event</button>
       <div style={{ position: 'relative', zIndex: 0 }}>
         <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin]} // Dodane
+          plugins={[dayGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
           ref={calendarRef}
           events={events}
-          dateClick={handleDateClick} // Dodane
-          // eventAdd={(event) => handleEventAdd(event)}
+          dateClick={handleDateClick}
           eventAdd={handleEventAdd}
-          // datesSet={(date) => handleDatesSet(date)}
+          eventRemove={handleEventDelete}
           datesSet={handleDateSet}
+          eventClick={(info) => {
+            setSelectedEvent(info.event);
+            setDeleteConfirmationOpen(true);
+            setOverlay(true);
+          }}
         />
       </div>
       <AddEventModal
         isOpen={modalOpen}
-        selectedDate={selectedDate} // Dodane
+        selectedDate={selectedDate}
         onClose={() => {
           setModalOpen(false);
-          setSelectedDate(null); // Reset daty po zamknięciu modala
+          setSelectedDate(null);
         }}
         onEventAdded={onEventAdded}
       />
+      {overlay && <div className="overlay"></div>}
+      {deleteConfirmationOpen && (
+        <div className="delete-confirmation">
+          <p>
+            Czy jesteś pewien że chcesz{' '}
+            <span style={{ color: 'red', fontWeight: 'bold' }}>usunać</span>{' '}
+            pobyt {selectedEvent != null && selectedEvent._def.title} w dniach
+            od:{' '}
+            {selectedEvent != null &&
+              moment(selectedEvent._instance.range.start).format(
+                'YYYY-MM-DD'
+              )}{' '}
+            do
+            {selectedEvent != null &&
+              moment(selectedEvent._instance.range.end).format(
+                'YYYY-MM-DD'
+              )}{' '}
+          </p>
+          <div className="delete-confirmation_btn_wrapper">
+            <button
+              className="delete-confirmation_btn"
+              onClick={{ handleEventDelete }}
+              style={{ backgroundColor: 'red' }}
+            >
+              Usuń
+            </button>
+            <button
+              className="delete-confirmation_btn"
+              onClick={() => {
+                setDeleteConfirmationOpen(false);
+                setOverlay(false);
+              }}
+              style={{ backgroundColor: 'blue' }}
+            >
+              Anuluj
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
--- server/Controllers/CalendarController.js
+++ server/Controllers/CalendarController.js
@@ -16,4 +16,21 @@ router.get('/get-events', async (req, res) => {
   res.send(events);
 });
 
+router.delete('/delete-event/:eventId', async (req, res) => {
+  try {
+    const eventId = req.params.eventId;
+    const deletedEvent = await Event.findByIdAndDelete(eventId);
+
+    if (deletedEvent) {
+      res.status(200).send({ message: 'Event deleted successfully' });
+    } else {
+      res.status(404).send({ message: 'Event not found' });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: 'Error deleting event', error: error.message });
+  }
+});
+
 module.exports = router;
