15509ca30384dce953cddea96ac39dcc1f114ad9
molszewski34 <m.olszewski341@gmail.com>
2024-01-10 12:28:24 +0100

    style, refactor: changed styles, refactored repeating elements
    style: buttons and display area
    refactor: squized repeating divs with same styles into one with new object

--- client/src/Components/modals/ManageRoomsModal.jsx
+++ client/src/Components/modals/ManageRoomsModal.jsx
@@ -2,8 +2,10 @@ import React, { useContext, useEffect, useState } from 'react';
 import { EventContext } from '../../contexts/event.context';
 import axios from 'axios';
 import { useQuery } from 'react-query';
-import { Button, Box, Text } from '@mui/material';
+import { Button, Box, Text, Typography } from '@mui/material';
 import AddRoom from '../manageRoomsModalComponents/addRoom';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 const ManageRoomsModal = () => {
   const { rooms, setRooms } = useContext(EventContext);
   const axiosInstance = axios.create({
@@ -26,6 +28,18 @@ const ManageRoomsModal = () => {
     setSelectedRoom(room);
   };
 
+  const roomDetailsData = selectedRoom
+    ? [
+        { label: 'Nazwa pokoju:', value: selectedRoom.roomName },
+        { label: 'Liczba miejsc:', value: selectedRoom.roomNumOfGuests },
+        { label: 'Cena za osobę:', value: selectedRoom.RoomPriceOfGuest },
+        { label: 'Kolor pokoju:', value: selectedRoom.roomColor },
+        { label: 'Lokalizacja:', value: selectedRoom.roomLocation },
+      ]
+    : [];
+
+  console.log(rooms);
+
   return (
     <Box
       sx={{
@@ -34,7 +48,6 @@ const ManageRoomsModal = () => {
         position: 'fixed',
         zIndex: 2,
         left: '50%',
-
         top: '50%',
         transform: 'translate(-50%, -50%)',
         border: '1px solid rgb(0 0 0 / 0.3)',
@@ -42,7 +55,6 @@ const ManageRoomsModal = () => {
         padding: '1em',
         boxShadow:
           '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
-
         backgroundColor: '#fff',
         minHeight: '410px',
       }}
@@ -62,11 +74,11 @@ const ManageRoomsModal = () => {
             justifyContent: 'space-between',
           }}
         >
-          <h2>Zarządzaj pokojami</h2>
+          <b>Zarządzaj pokojami</b>
           <Button
             variant="contained"
             style={{
-              fontSize: '0.7em',
+              fontSize: '.7em',
 
               fontWeight: 'bold',
             }}
@@ -95,9 +107,16 @@ const ManageRoomsModal = () => {
               <Button
                 key={room.id}
                 size="small"
-                variant={selectedRoom === room ? 'contained' : 'outlined'}
                 onClick={() => handleRoomButtonClick(room)}
-                style={{ fontSize: '0.7em' }}
+                style={{
+                  fontSize: '0.7em',
+
+                  border:
+                    selectedRoom === room
+                      ? '1px solid #6b7280'
+                      : '1px solid #d1d5db',
+                  color: '#6b7280',
+                }}
               >
                 {room.roomName}
               </Button>
@@ -112,76 +131,112 @@ const ManageRoomsModal = () => {
                 flexDirection: 'column',
                 fontSize: '0.8em',
                 marginLeft: '.5em',
+                justifyContent: 'space-between',
+                minWidth: '120px',
               }}
             >
-              <div
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  gap: '1em',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <p style={{ margin: '0' }}>Liczba gości</p>
-                <span>{selectedRoom.roomNumOfGuests}</span>
-              </div>
-              <div
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  gap: '1em',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <p style={{ margin: '0' }}>Cena za osobę</p>
-                <span>{selectedRoom.RoomPriceOfGuest}</span>
-              </div>
-              <div
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  gap: '1em',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <p style={{ margin: '0' }}>Kolor pokoju</p>
-                <div
-                  style={{
-                    width: '20px',
-                    height: '20px',
-                    backgroundColor: selectedRoom.roomColor,
+              <Box>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    margin: '0',
+                    marginBottom: '0.8em',
+                    fontWeight: 'bold',
+                    fontSize: '1.4em',
                   }}
-                ></div>
-              </div>
-              <div
-                style={{
+                >
+                  Dane pokoju
+                </Typography>
+                {roomDetailsData.map((roomData) => (
+                  <Box
+                    key={roomData.label}
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <Typography
+                      variant="body2"
+                      sx={{ margin: '0', fontWeight: 'bold' }}
+                    >
+                      {roomData.label}
+                    </Typography>
+                    {roomData.label === 'Kolor pokoju:' ? (
+                      <Box
+                        sx={{
+                          backgroundColor: selectedRoom.roomColor,
+                          width: '20px',
+                          height: '20px',
+                          borderRadius: '15px',
+                        }}
+                      ></Box>
+                    ) : (
+                      <Typography
+                        style={{
+                          color:
+                            roomData.label === 'Nazwa pokoju:'
+                              ? '#0284c7'
+                              : '#4b5563',
+                          fontWeight:
+                            roomData.label === 'Nazwa pokoju:' ? 'bolder' : '',
+                        }}
+                        variant="body1"
+                      >
+                        {roomData.value}
+                      </Typography>
+                    )}
+                  </Box>
+                ))}
+              </Box>
+              <Box
+                sx={{
                   display: 'flex',
-                  alignItems: 'center',
-                  gap: '1em',
                   justifyContent: 'space-between',
+                  gap: '1em',
                 }}
               >
-                <p style={{ margin: '0' }}>Cena za osobę</p>
-                <span>{selectedRoom.RoomPriceOfGuest}</span>
-              </div>
-              <div
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  gap: '1em',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <p style={{ margin: '0' }}>Lokalizacja</p>
-                <span>{selectedRoom.roomLocation}</span>
-              </div>
+                <Button
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: '#0ea5e9',
+                    border: '1px solid #d1d5db',
+                    borderRadius: '5px',
+                    padding: '0.5em',
+                  }}
+                >
+                  <EditIcon style={{ fontSize: '1.5em', margin: '0' }} />
+                  <p style={{ fontSize: '.8em', margin: '0' }}>Edytuj</p>
+                </Button>
+
+                <Button
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    color: '#ef4444',
+                    border: '1px solid #d1d5db',
+                    borderRadius: '5px',
+                    padding: '0.5em',
+                  }}
+                >
+                  <DeleteForeverIcon
+                    style={{ fontSize: '1.5em', margin: '0' }}
+                  />
+                  <p style={{ fontSize: '.8em', margin: '0' }}>Usuń</p>
+                </Button>
+              </Box>
             </Box>
           ) : (
-            <p>Wybierz pokój</p>
+            <p style={{ padding: ' 0 .3em' }}>Wybierz pokój</p>
           )}
+          {isOpen && <AddRoom />}
         </Box>
       </Box>
-      {isOpen && <AddRoom />}
     </Box>
   );
 };
