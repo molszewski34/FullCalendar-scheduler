7cbac080a0b5a95ca95fe4da8030f86a7c8f3792
molszewski34 <m.olszewski341@gmail.com>
2024-01-15 09:47:28 +0100

    feat: added new states, open/close feature, conditionaly rendered rooms and details

--- client/src/Components/modals/ManageRoomsModal.jsx
+++ client/src/Components/modals/ManageRoomsModal.jsx
@@ -5,11 +5,19 @@ import { useQuery } from 'react-query';
 import { Button, Box, Text, Typography } from '@mui/material';
 import AddRoom from '../manageRoomsModalComponents/addRoom';
 import EditRoom from '../manageRoomsModalComponents/editRoom';
+import DeleteRoom from '../manageRoomsModalComponents/deleteRoom';
 import EditIcon from '@mui/icons-material/Edit';
+import CloseIcon from '@mui/icons-material/Close';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 const ManageRoomsModal = () => {
-  const { rooms, setRooms, roomSelection, setRoomSelection } =
-    useContext(EventContext);
+  const {
+    rooms,
+    setRooms,
+    roomSelection,
+    setRoomSelection,
+    openManageRoomsModal,
+    setOpenManageRoomsModal,
+  } = useContext(EventContext);
   const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_PUBLIC_API_URL,
   });
@@ -19,6 +27,7 @@ const ManageRoomsModal = () => {
 
   const [openAddRoomPanel, setOpenAddRoomPanel] = useState(false);
   const [openEditRoomPanel, setOpenEditRoomPanel] = useState(false);
+  const [openDeleteRoomPanel, setOpenDeleteRoomPanel] = useState(false);
 
   useEffect(() => {
     if (roomsData) {
@@ -40,10 +49,13 @@ const ManageRoomsModal = () => {
       ]
     : [];
 
+  console.log(roomSelection);
+
   return (
     <Box
       sx={{
         display: 'flex',
+        flexDirection: 'column',
         gap: '.5em',
         position: 'fixed',
         zIndex: 2,
@@ -58,6 +70,10 @@ const ManageRoomsModal = () => {
         backgroundColor: '#fff',
       }}
     >
+      <CloseIcon
+        style={{ color: '#dc2626', fontSize: '1.6em', cursor: 'pointer' }}
+        onClick={() => setOpenManageRoomsModal(false)}
+      />
       <div
         style={{
           display: 'flex',
@@ -71,6 +87,7 @@ const ManageRoomsModal = () => {
             display: 'flex',
             marginBottom: '1em',
             justifyContent: 'space-between',
+            gap: '1em',
           }}
         >
           <b>Zarządzaj pokojami</b>
@@ -83,6 +100,7 @@ const ManageRoomsModal = () => {
             }}
             onClick={() => {
               setOpenAddRoomPanel(!openAddRoomPanel);
+              setOpenDeleteRoomPanel(false);
               setOpenEditRoomPanel(false);
             }}
           >
@@ -94,184 +112,208 @@ const ManageRoomsModal = () => {
             display: 'flex',
           }}
         >
-          <div
-            style={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(4, 1fr)',
+          {rooms.length > 0 ? (
+            <div
+              style={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(4, 1fr)',
 
-              gap: '0.2em',
-
-              alignContent: 'start',
-              maxWidth: '400px',
-            }}
-          >
-            {rooms.map((room) => (
-              <>
-                <Button
-                  key={room.id}
-                  size="small"
-                  onClick={() => handleRoomButtonClick(room)}
-                  style={{
-                    fontSize: '0.7em',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    width: '100%',
-                    height: '50px',
-
-                    border:
-                      roomSelection === room
-                        ? '1px solid #6b7280'
-                        : '1px solid #d1d5db',
-                    // borderRadius: '3px',
-                    color: '#6b7280',
-                  }}
-                >
-                  {room.roomName}
-                </Button>
-              </>
-            ))}
-          </div>
-
-          {roomSelection ? (
-            <Box
-              sx={{
-                display: 'flex',
                 gap: '0.2em',
-                flexDirection: 'column',
-                fontSize: '0.8em',
-                marginLeft: '.5em',
-                justifyContent: 'space-between',
-                minWidth: '120px',
+
+                alignContent: 'start',
+                maxWidth: '400px',
               }}
             >
-              <Box>
-                <Typography
-                  variant="h6"
+              {rooms.map((room) => (
+                <>
+                  <Button
+                    key={room.id}
+                    size="small"
+                    onClick={() => handleRoomButtonClick(room)}
+                    style={{
+                      fontSize: '0.7em',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      width: '100%',
+                      minHeight: '50px',
+                      color: '#000',
+                      fontWeight: ` ${
+                        roomSelection && roomSelection._id === room._id
+                          ? 'bold'
+                          : ''
+                      }`,
+                      // borderColor: room.roomColor,
+                      border: `2px solid  ${
+                        roomSelection && roomSelection._id === room._id
+                          ? '#000'
+                          : '#d4d4d8'
+                      }`,
+                    }}
+                  >
+                    {room.roomName}
+                  </Button>
+                </>
+              ))}
+            </div>
+          ) : (
+            <p style={{ padding: '0.3em', fontSize: '0.9em' }}>Dodaj pokoje</p>
+          )}
+          {rooms.length != 0 ? (
+            <div className="">
+              {roomSelection ? (
+                <Box
                   sx={{
-                    margin: '0',
-                    marginBottom: '0.8em',
-                    fontWeight: 'bolder',
-                    fontSize: '1.3em',
+                    display: 'flex',
+                    gap: '0.2em',
+                    flexDirection: 'column',
+                    fontSize: '0.8em',
+                    marginLeft: '.5em',
+                    justifyContent: 'space-between',
+
+                    width: '120px',
                   }}
                 >
-                  Dane pokoju
-                </Typography>
-                {roomDetailsData.map((roomData) => (
+                  <Box>
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        margin: '0',
+                        marginBottom: '0.8em',
+                        fontWeight: 'bolder',
+                        fontSize: '1.3em',
+                      }}
+                    >
+                      Dane pokoju
+                    </Typography>
+                    {roomDetailsData.map((roomData) => (
+                      <Box
+                        key={roomData.label}
+                        sx={{
+                          display: 'flex',
+                          flexDirection: 'column',
+                          justifyContent: 'space-between',
+                        }}
+                      >
+                        <Typography
+                          variant="body1"
+                          sx={{
+                            margin: '0',
+
+                            fontWeight: '700',
+                            fontSize: '1em',
+                          }}
+                        >
+                          {roomData.label}
+                        </Typography>
+                        {roomData.label === 'Kolor pokoju:' ? (
+                          <Box
+                            sx={{
+                              backgroundColor: roomSelection.roomColor,
+                              width: '20px',
+                              height: '20px',
+                              borderRadius: '15px',
+                            }}
+                          ></Box>
+                        ) : (
+                          <Typography
+                            style={{
+                              fontSize: '1.1em',
+                              color:
+                                roomData.label === 'Nazwa pokoju:'
+                                  ? '#ea580c'
+                                  : '#71717a',
+                              fontWeight:
+                                roomData.label === 'Nazwa pokoju:'
+                                  ? 'bolder'
+                                  : '',
+                            }}
+                            variant="body1"
+                          >
+                            {roomData.value}
+                          </Typography>
+                        )}
+                      </Box>
+                    ))}
+                  </Box>
                   <Box
-                    key={roomData.label}
                     sx={{
                       display: 'flex',
                       flexDirection: 'column',
                       justifyContent: 'space-between',
+                      gap: '.5em',
                     }}
                   >
-                    <Typography
-                      variant="body1"
-                      sx={{
-                        margin: '0',
+                    <button
+                      style={{
+                        display: 'flex',
 
-                        fontWeight: '800',
-                        fontSize: '1em',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: '#0ea5e9',
+                        color: '#fff',
+                        fontWeight: 'bold',
+                        border: '1px solid #0284c7',
+                        borderRadius: '5px',
+                        padding: '0.5em',
+                        boxShadow: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+                        cursor: 'pointer',
+                      }}
+                      onClick={() => {
+                        setOpenEditRoomPanel(!openEditRoomPanel);
+                        setOpenDeleteRoomPanel(false);
+                        setOpenAddRoomPanel(false);
                       }}
                     >
-                      {roomData.label}
-                    </Typography>
-                    {roomData.label === 'Kolor pokoju:' ? (
-                      <Box
-                        sx={{
-                          backgroundColor: roomSelection.roomColor,
-                          width: '20px',
-                          height: '20px',
-                          borderRadius: '15px',
-                        }}
-                      ></Box>
-                    ) : (
-                      <Typography
+                      <EditIcon style={{ fontSize: '1.5em', margin: '0' }} />
+                      <p
                         style={{
-                          fontSize: '1.1em',
-                          color:
-                            roomData.label === 'Nazwa pokoju:'
-                              ? '#44403c'
-                              : '#71717a',
-                          fontWeight:
-                            roomData.label === 'Nazwa pokoju:' ? 'bolder' : '',
+                          fontSize: '.8em',
+                          margin: '0',
                         }}
-                        variant="body1"
                       >
-                        {roomData.value}
-                      </Typography>
-                    )}
+                        Edytuj
+                      </p>
+                    </button>
+
+                    <button
+                      style={{
+                        display: 'flex',
+
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: '#ef4444',
+                        color: '#fff',
+                        fontWeight: 'bold',
+                        border: '1px solid #dc2626',
+                        borderRadius: '5px',
+                        padding: '0.5em',
+                        boxShadow: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+                        cursor: 'pointer',
+                      }}
+                      onClick={() => {
+                        setOpenDeleteRoomPanel(!openDeleteRoomPanel);
+                        setOpenEditRoomPanel(false);
+                        setOpenAddRoomPanel(false);
+                      }}
+                    >
+                      <DeleteForeverIcon
+                        style={{ fontSize: '1.5em', margin: '0' }}
+                      />
+                      <p style={{ fontSize: '.8em', margin: '0' }}>Usuń</p>
+                    </button>
                   </Box>
-                ))}
-              </Box>
-              <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'space-between',
-                  gap: '.5em',
-                }}
-              >
-                <button
-                  style={{
-                    display: 'flex',
-
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: '#0ea5e9',
-                    color: '#fff',
-                    fontWeight: 'bold',
-                    border: '1px solid #0284c7',
-                    borderRadius: '5px',
-                    padding: '0.5em',
-                    boxShadow: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
-                    cursor: 'pointer',
-                  }}
-                  onClick={() => {
-                    setOpenEditRoomPanel(!openEditRoomPanel);
-                    setOpenAddRoomPanel(false);
-                  }}
-                >
-                  <EditIcon style={{ fontSize: '1.5em', margin: '0' }} />
-                  <p
-                    style={{
-                      fontSize: '.8em',
-                      margin: '0',
-                    }}
-                  >
-                    Edytuj
-                  </p>
-                </button>
-
-                <button
-                  style={{
-                    display: 'flex',
-
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    backgroundColor: '#ef4444',
-                    color: '#fff',
-                    fontWeight: 'bold',
-                    border: '1px solid #dc2626',
-                    borderRadius: '5px',
-                    padding: '0.5em',
-                    boxShadow: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
-                    cursor: 'pointer',
-                  }}
-                >
-                  <DeleteForeverIcon
-                    style={{ fontSize: '1.5em', margin: '0' }}
-                  />
-                  <p style={{ fontSize: '.8em', margin: '0' }}>Usuń</p>
-                </button>
-              </Box>
-            </Box>
+                </Box>
+              ) : (
+                <p style={{ padding: ' 0 .3em' }}>Wybierz pokój</p>
+              )}
+            </div>
           ) : (
-            <p style={{ padding: ' 0 .3em' }}>Wybierz pokój</p>
+            ''
           )}
           {openAddRoomPanel && <AddRoom />}
           {openEditRoomPanel && <EditRoom />}
+          {openDeleteRoomPanel && <DeleteRoom />}
         </Box>
       </div>
     </Box>
