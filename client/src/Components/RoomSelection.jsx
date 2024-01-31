d95f0b8658aa9c76835cd6f9e012591d91afc37a
molszewski34 <m.olszewski341@gmail.com>
2024-01-31 09:08:13 +0100

    feat: moved room selection feature to roomSelectionModal.jsx, added button to chosse room
    Button when clicked switches to roomName and become green

--- client/src/Components/RoomSelection.jsx
+++ client/src/Components/RoomSelection.jsx
@@ -1,35 +1,8 @@
 import React, { useContext } from 'react';
 import { EventContext } from '../contexts/event.context';
-import { useForm, Controller } from 'react-hook-form';
 
-const RoomSelection = ({
-  room,
-  error,
-  setRoom,
-  setColor,
-  setNumOfGuests,
-  setPriceOfGuest,
-  setSelectedRoom,
-}) => {
-  const { setGuestsFee, setTotal, roomsList } = useContext(EventContext);
-
-  const { handleSubmit, control, setError } = useForm();
-
-  console.log(roomsList);
-
-  const handleRoomChange = (roomItem) => {
-    setRoom(roomItem.name);
-    setColor(roomItem.color);
-    setNumOfGuests(roomItem.numOfGuests);
-    setPriceOfGuest(roomItem.priceOfGuest);
-    setSelectedRoom(roomItem.name);
-  };
-
-  const generateInputs = (count) => {
-    const newInputs = Array.from({ length: count }, () => 65);
-    setGuestsFee(newInputs);
-    setTotal(newInputs.length * 65);
-  };
+const RoomSelection = ({ room }) => {
+  const { setOpenRoomSelectionModal } = useContext(EventContext);
 
   return (
     <div>
@@ -37,23 +10,22 @@ const RoomSelection = ({
       {room === 'default' && <p className="error"> Wybierz 1 pokój</p>}
       <div className="room-selection">
         <div className="room-list">
-          {roomsList.map((roomItem, index) => (
-            <button
-              key={index}
-              type="button"
-              className={`room-button ${
-                room === roomItem.name ? 'selected' : ''
-              }`}
-              style={{ background: roomItem.color }}
-              onClick={() => {
-                handleRoomChange(roomItem);
-
-                generateInputs(roomItem.defNumOfGuests);
-              }}
-            >
-              {roomItem.name}
-            </button>
-          ))}
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              setOpenRoomSelectionModal(true);
+            }}
+            style={{
+              border: `2px solid ${
+                room != 'Wybierz pokój' ? '#65a30d' : '#000'
+              }`,
+              background: '#fff',
+              color: room != 'Wybierz pokój' ? '#65a30d' : '#000',
+            }}
+            className="room-button"
+          >
+            {room}
+          </button>
         </div>
       </div>
     </div>
