af6e6ec10d03989d241e6c449ebb5359073232ff
molszewski34 <m.olszewski341@gmail.com>
2024-02-02 08:29:11 +0100

    refactor: moved discount panel functions and html into new component

--- client/src/Components/modals/AddEventModal.jsx
+++ client/src/Components/modals/AddEventModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import Form from '../Form';
-import { Button, Box } from '@mui/material';
+import { Box } from '@mui/material';
 import { EventContext } from '../../contexts/event.context';
 import {
   handleNumOfGuestsIncrement,
@@ -10,10 +10,9 @@ import {
   handlePriceOfGuestDecrement,
 } from '../utilities/eventUtilities';
 import Header from '../Header';
-import PersonIcon from '@mui/icons-material/Person';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+
 import RoomSelectionModal from './roomSelectionModal';
+import DiscountPanel from '../DiscountPanel';
 
 const AddEventModal = ({ isOpen, onClose, onEventAdded }) => {
   const {
@@ -39,15 +38,8 @@ const AddEventModal = ({ isOpen, onClose, onEventAdded }) => {
     daysDifference,
     setDaysDifference,
     setOverlay,
-    guestsFee,
-    setGuestsFee,
     total,
-    setTotal,
-    initialInputs,
-    setInitialInputs,
-    discountBtns,
     openRoomSelectionModal,
-    setOpenRoomSelectionModal,
   } = useContext(EventContext);
 
   const [selectedRoom, setSelectedRoom] = useState('');
@@ -72,76 +64,9 @@ const AddEventModal = ({ isOpen, onClose, onEventAdded }) => {
     onClose();
   };
 
-  console.log(guestsFee);
-
   const {
     formState: { errors },
   } = useForm();
-
-  const handleInputChange = (index, value) => {
-    if (value < 0) {
-      value = 0;
-    }
-    const newInputValues = [...guestsFee];
-    newInputValues[index] = parseFloat(value);
-    const sum = newInputValues.reduce(
-      (acc, currentValue) => acc + currentValue,
-      0
-    );
-
-    setGuestsFee(newInputValues);
-    setTotal(sum);
-  };
-
-  const incrementValue = (index) => {
-    const newInputValues = [...guestsFee];
-    newInputValues[index] = (parseFloat(newInputValues[index]) || 0) + 1;
-    const sum = newInputValues.reduce(
-      (acc, currentValue) => acc + currentValue,
-      0
-    );
-    setTotal(sum);
-    setGuestsFee(newInputValues);
-    setInitialInputs(guestsFee);
-  };
-
-  const decrementValue = (index) => {
-    const newInputValues = [...guestsFee];
-    newInputValues[index] = Math.max(
-      (parseFloat(newInputValues[index]) || 0) - 1,
-      0
-    );
-    const sum = newInputValues.reduce(
-      (acc, currentValue) => acc + currentValue,
-      0
-    );
-    setTotal(sum);
-    setGuestsFee(newInputValues);
-    setInitialInputs(guestsFee);
-  };
-
-  const changeValuePercentage = (index, percentage) => {
-    if (guestsFee === initialInputs) {
-      const newInputs = [...guestsFee];
-      const newValue = (newInputs[index] * (1 + percentage / 100)).toFixed(2);
-
-      const diff = newValue - newInputs[index];
-      newInputs[index] = parseFloat(newValue);
-
-      setGuestsFee(newInputs);
-      setTotal(total + parseFloat(diff));
-    } else {
-      const initialValue = [...initialInputs];
-      const newInputs = [...guestsFee];
-      const newValue = (initialValue[index] * (1 + percentage / 100)).toFixed(
-        2
-      );
-      const diff = newValue - newInputs[index];
-      newInputs[index] = parseFloat(newValue);
-      setGuestsFee(newInputs);
-      setTotal(total + parseFloat(diff));
-    }
-  };
 
   return (
     <>
@@ -192,77 +117,7 @@ const AddEventModal = ({ isOpen, onClose, onEventAdded }) => {
             ></Form>
           </Box>
 
-          <div
-            className="discount-container"
-            style={{
-              height: '600px',
-              width: '300px',
-
-              overflowY: 'auto',
-              borderBottom: `${
-                guestsFee.length >= 7 ? '1px solid #a1a1aa' : ''
-              } `,
-              paddingBottom: `${guestsFee.length >= 7 ? '0.8em' : ''} `,
-              scrollBehavior: 'smooth',
-              overflowScrolling: 'touch',
-              overscrollBehaviorY: 'none',
-            }}
-          >
-            {guestsFee.map((value, index) => (
-              <div className="discount-box">
-                <div className="discount-box-wrapper">
-                  <div className="discount-box-item" key={index}>
-                    <PersonIcon style={{ color: '#757575' }} />
-                    <input
-                      key={index}
-                      type="text"
-                      value={value}
-                      style={{
-                        fontSize: '1em',
-                        color: ` ${
-                          value[index] <= 0 || value[index] <= '0' ? 'red' : ''
-                        }`,
-                        border: `solid 1px ${
-                          value[index] <= 0 || value[index] <= '0' ? 'red' : ''
-                        }`,
-                      }}
-                      onChange={(e) => handleInputChange(index, e.target.value)}
-                    />
-                    <div className="discount-box-item-btns">
-                      <RemoveCircleOutlineIcon
-                        fontSize="large"
-                        type="button"
-                        onClick={() => decrementValue(index)}
-                        color="warning"
-                      />
-                      <AddCircleOutlineIcon
-                        fontSize="large"
-                        type="button"
-                        onClick={() => incrementValue(index)}
-                        style={{ color: '#8bc34a' }}
-                      />
-                    </div>
-                  </div>
-                  <div className="discount-btns ">
-                    {discountBtns.map((discountBtn) => (
-                      <Button
-                        variant="contained"
-                        style={{ backgroundColor: discountBtn.bgColor }}
-                        className="discount-btn"
-                        sx={{ my: 1 }}
-                        size="small"
-                        onClick={() =>
-                          changeValuePercentage(index, discountBtn.value)
-                        }
-                      >
-                        {discountBtn.name}
-                      </Button>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+          <DiscountPanel />
           {openRoomSelectionModal && <RoomSelectionModal />}
         </div>
       )}
