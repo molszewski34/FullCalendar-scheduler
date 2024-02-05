edbfd05dddca0d5b7b4ecbd635c6483c9b83cb2a
molszewski34 <m.olszewski341@gmail.com>
2024-02-05 10:55:30 +0100

    refactor: separated styles in style to css file

--- client/src/Components/modals/ManageRoomsModal.jsx
+++ client/src/Components/modals/ManageRoomsModal.jsx
@@ -9,6 +9,7 @@ import DeleteRoom from '../manageRoomsModalComponents/deleteRoom';
 import EditIcon from '@mui/icons-material/Edit';
 import CloseIcon from '@mui/icons-material/Close';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import './styles/ManageRoomsModal.css';
 const ManageRoomsModal = () => {
   const {
     rooms,
@@ -49,55 +50,18 @@ const ManageRoomsModal = () => {
       ]
     : [];
 
-  console.log(roomSelection);
-
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '.5em',
-        position: 'fixed',
-        zIndex: 2,
-        left: '50%',
-        top: '50%',
-        transform: 'translate(-50%, -50%)',
-        border: '1px solid rgb(0 0 0 / 0.3)',
-        borderRadius: '5px',
-        padding: '1em',
-        boxShadow:
-          '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
-        backgroundColor: '#fff',
-      }}
-    >
+    <Box className="manage-rooms-modal-container">
       <CloseIcon
         style={{ color: '#dc2626', fontSize: '1.6em', cursor: 'pointer' }}
         onClick={() => setOpenManageRoomsModal(false)}
       />
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '0.2em',
-          flexDirection: 'column',
-        }}
-      >
-        <Box
-          style={{
-            display: 'flex',
-            marginBottom: '1em',
-            justifyContent: 'space-between',
-            gap: '1em',
-          }}
-        >
+      <div className="manage-rooms-modal-wrapper">
+        <Box className="manage-rooms-modal-header">
           <b>Zarządzaj pokojami</b>
           <Button
             variant="contained"
-            style={{
-              fontSize: '.7em',
-
-              fontWeight: 'bold',
-            }}
+            className="manage-rooms-modal-add-button"
             onClick={() => {
               setOpenAddRoomPanel(!openAddRoomPanel);
               setOpenDeleteRoomPanel(false);
@@ -113,37 +77,24 @@ const ManageRoomsModal = () => {
           }}
         >
           {rooms.length > 0 ? (
-            <div
-              style={{
-                display: 'grid',
-                gridTemplateColumns: 'repeat(4, 1fr)',
-
-                gap: '0.2em',
-
-                alignContent: 'start',
-                maxWidth: '400px',
-              }}
-            >
+            <div className="room-buttons-container">
               {rooms.map((room) => (
                 <>
                   <Button
                     key={room.id}
                     size="small"
                     onClick={() => handleRoomButtonClick(room)}
+                    className="room-button"
                     style={{
                       fontSize: '0.7em',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      width: '100%',
-                      minHeight: '50px',
                       color: '#000',
+
                       fontWeight: ` ${
                         roomSelection && roomSelection._id === room._id
                           ? 'bold'
                           : ''
                       }`,
-                      // borderColor: room.roomColor,
+
                       border: `2px solid  ${
                         roomSelection && roomSelection._id === room._id
                           ? '#000'
@@ -245,20 +196,7 @@ const ManageRoomsModal = () => {
                     }}
                   >
                     <button
-                      style={{
-                        display: 'flex',
-
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: '#0ea5e9',
-                        color: '#fff',
-                        fontWeight: 'bold',
-                        border: '1px solid #0284c7',
-                        borderRadius: '5px',
-                        padding: '0.5em',
-                        boxShadow: '0 1px 2px 0 rgb(0 0 0 / 0.05)',
-                        cursor: 'pointer',
-                      }}
+                      className="room-action-button"
                       onClick={() => {
                         setOpenEditRoomPanel(!openEditRoomPanel);
                         setOpenDeleteRoomPanel(false);
