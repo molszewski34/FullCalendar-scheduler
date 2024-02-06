169485972d66008860b04d6bbe18ee47deddad5e
Mariusz Olszewski <85546516+molszewski34@users.noreply.github.com>
2024-02-06 08:16:57 +0100

    fix: file name change

---
+++ client/src/Components/modals/RoomSelectionModal.jsx
@@ -0,0 +1,91 @@
+import React, { useContext } from 'react';
+import { EventContext } from '../../contexts/event.context';
+import CloseIcon from '@mui/icons-material/Close';
+import LocalHotelIcon from '@mui/icons-material/LocalHotel';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import '../styles/RoomSelectionModal.css';
+const RoomSelectionModal = () => {
+  const {
+    setRoom,
+    rooms,
+    setOpenRoomSelectionModal,
+    setRoomSelection,
+    setGuestsFee,
+    setInitialInputs,
+    setPriceOfGuest,
+    setNumOfGuests,
+    setTotal,
+    setColor,
+    setRoomId,
+  } = useContext(EventContext);
+
+  const handleRoomButtonClick = (room) => {
+    setRoomSelection({ ...room });
+    setRoom(room.roomName);
+    setPriceOfGuest(room.RoomPriceOfGuest);
+    setNumOfGuests(room.roomNumOfGuests);
+    setColor(room.roomColor);
+    setRoomId(room._id);
+  };
+
+  const generateInputs = (count, priceOfGuest) => {
+    const newInputs = Array.from({ length: count }, () => priceOfGuest);
+    setGuestsFee(newInputs);
+    setInitialInputs(newInputs);
+    setTotal(newInputs.length * priceOfGuest);
+  };
+
+  return (
+    <div className="modal-room-selection">
+      <div className="modal-room-selection_header">
+        <p>Wybierz pokój</p>
+        <CloseIcon onClick={() => setOpenRoomSelectionModal(false)} />
+      </div>
+      <div
+        className="modal-room-selection_wrapper"
+        style={{
+          borderBottom: `${
+            (rooms?.length ?? 0) >= 16 ? '1px solid #a1a1aa' : ''
+          } `,
+          paddingBottom: `${(rooms?.length ?? 0) >= 16 ? '0.8em' : ''} `,
+        }}
+      >
+        {rooms?.map((room) => (
+          <>
+            <button
+              key={room._id}
+              role="button"
+              onClick={(e) => {
+                handleRoomButtonClick(room);
+                setOpenRoomSelectionModal(false);
+                generateInputs(room.roomNumOfGuests, room.RoomPriceOfGuest);
+              }}
+              style={{ borderColor: room.roomColor }}
+            >
+              <div className="modal-room-selection_info__top">
+                <span>
+                  <LocalHotelIcon />
+                  {room.roomNumOfGuests}
+                </span>
+                <span>
+                  <AttachMoneyIcon />
+                  {room.RoomPriceOfGuest}zł
+                </span>
+              </div>
+              {room.roomName}
+              <div className="modal-room-selection_info__bottom">
+                <span>
+                  <LocationOnIcon />
+                  {room.roomLocation}
+                </span>
+              </div>
+            </button>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RoomSelectionModal;
