fb0bcdb4993b1be52edd8460a87ffc3285b8540c
molszewski34 <m.olszewski341@gmail.com>
2024-02-08 09:13:11 +0100

    feat: created validation for all fields

--- client/src/Components/manageRoomsModalComponents/addRoom.jsx
+++ client/src/Components/manageRoomsModalComponents/addRoom.jsx
@@ -1,62 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { Button } from '@mui/material';
 import { ChromePicker } from 'react-color';
-
+import { useForm } from 'react-hook-form';
+import CloseIcon from '@mui/icons-material/Close';
+import { EventContext } from '../../contexts/event.context';
 import AddIcon from '@mui/icons-material/Add';
 
 const AddRoom = () => {
+  const { showColorPicker, setShowColorPicker } = useContext(EventContext);
   const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_PUBLIC_API_URL,
   });
-  const [roomData, setRoomData] = useState({
-    roomName: '',
-    roomNumOfGuests: 0,
-    RoomPriceOfGuest: 0,
-    isApartment: false,
-    roomColor: '',
-    roomLocation: '',
-    events: [],
-  });
-  const [showColorPicker, setShowColorPicker] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const [roomColor, setRoomColor] = useState('#cbd5e1');
+
   const handleColorChange = (color) => {
-    setRoomData({
-      ...roomData,
-      roomColor: color.hex,
-    });
+    setRoomColor(color.hex);
   };
 
-  const handleChange = async (e) => {
-    setRoomData({
-      ...roomData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const onSubmit = async (data) => {
     try {
+      const formData = { ...data, roomColor: roomColor };
       const response = await axiosInstance.post(
         '/api/rooms/create-room',
-        roomData
+        formData
       );
 
       console.log('Data added successfully:', response.data);
 
-      setRoomData({
-        roomName: '',
-        roomNumOfGuests: 0,
-        RoomPriceOfGuest: 0,
-        isApartment: false,
-        roomColor: '',
-        roomLocation: '',
-        events: [],
-      });
+      reset();
+      setRoomColor('#cbd5e1');
     } catch (error) {
       console.error('Error adding data:', error);
     }
   };
+
+  console.log(roomColor);
 
   return (
     <div
@@ -70,48 +55,75 @@ const AddRoom = () => {
       }}
     >
       <b style={{ margin: '0', marginBottom: '1em' }}>Wypełnij pola</b>
-      <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
+      <form style={{ display: 'flex' }} onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="roomName">Nazwa pokoju:</label>
         <input
           type="text"
           id="roomName"
-          name="roomName"
-          value={roomData.roomName}
-          onChange={handleChange}
+          {...register('roomName', {
+            required: 'Nazwa pokoju jest wymagana',
+            minLength: {
+              value: 3,
+              message: 'Imię musi mieć co najmniej 3 znako',
+            },
+            maxLength: {
+              value: 30,
+              message: 'Imię nie może mieć więcej niż 20 znaków',
+            },
+          })}
           placeholder="np. Pokój 3"
         />
+        {errors.roomName && (
+          <span className="error">{errors.roomName.message}</span>
+        )}
 
         <label htmlFor="roomNumOfGuests">Liczba miejsc:</label>
         <input
           type="text"
           id="roomNumOfGuests"
-          name="roomNumOfGuests"
-          value={roomData.roomNumOfGuests}
-          onChange={handleChange}
+          {...register('roomNumOfGuests', {
+            required: 'Liczba miejsc jest wymagana',
+            min: {
+              value: 1,
+              message: 'Liczba miejsc nie może być mniejsza niż 1',
+            },
+            max: {
+              value: 9999,
+              message: 'Przekroczono maksymalną liczbę osób',
+            },
+            pattern: {
+              value: /^[0-9]*$/,
+              message: 'Pole może zawierać tylko liczby',
+            },
+          })}
+          placeholder="np. 3"
         />
+        {errors.roomNumOfGuests && (
+          <span className="error">{errors.roomNumOfGuests.message}</span>
+        )}
 
-        <label htmlFor="RoomPriceOfGuest">Cena za osobę</label>
+        <label htmlFor="RoomPriceOfGuest">Cena za osobę:</label>
         <input
           type="text"
           id="RoomPriceOfGuest"
-          name="RoomPriceOfGuest"
-          value={roomData.RoomPriceOfGuest}
-          onChange={handleChange}
+          {...register('RoomPriceOfGuest', {
+            required: 'Cena za osobę jest wymagana',
+            min: {
+              value: 1,
+              message: 'Liczba miejsc nie może być mniejsza niż 1',
+            },
+            pattern: {
+              value: /^[0-9]*$/,
+              message: 'Pole może zawierać tylko liczby',
+            },
+          })}
+          placeholder="np. 60"
         />
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <label htmlFor="isApartment">Czy jest Apartamentem?</label>
-          <input
-            type="checkbox"
-            id="isApartment"
-            name="isApartment"
-            checked={roomData.isApartment}
-            onChange={() =>
-              setRoomData({ ...roomData, isApartment: !roomData.isApartment })
-            }
-          />
-        </div>
-        <label htmlFor="roomColor">Kolor pokoju:</label>
+        {errors.RoomPriceOfGuest && (
+          <span className="error">{errors.RoomPriceOfGuest.message}</span>
+        )}
 
+        <label htmlFor="roomColor">Kolor pokoju:</label>
         <div
           style={{
             display: 'flex',
@@ -119,32 +131,31 @@ const AddRoom = () => {
             position: 'relative',
           }}
         >
-          <input
-            type="text"
-            id="roomColor"
-            name="roomColor"
-            value={roomData.roomColor}
-            onChange={handleChange}
-            placeholder="Kliknij przycisk"
-          />
+          <input type="text" id="roomColor" value={roomColor} />
+
           <div
             style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
               width: '40px',
               height: '30px',
               borderRadius: '5px',
               cursor: 'pointer',
-              backgroundColor:
-                roomData.roomColor === '' ? '#cbd5e1' : roomData.roomColor,
+              fontSize: '0.5em',
+              backgroundColor: roomColor,
             }}
             onClick={() => setShowColorPicker(!showColorPicker)}
-          ></div>
+          >
+            {showColorPicker ? <CloseIcon /> : ''}
+          </div>
         </div>
+        {errors.roomColor && (
+          <span className="error">{errors.roomColor.message}</span>
+        )}
         {showColorPicker && (
           <div style={{ position: 'absolute', bottom: '-130px', zIndex: '1' }}>
-            <ChromePicker
-              color={roomData.roomColor}
-              onChange={handleColorChange}
-            />
+            <ChromePicker color={roomColor} onChange={handleColorChange} />
           </div>
         )}
 
@@ -152,11 +163,22 @@ const AddRoom = () => {
         <input
           type="text"
           id="roomLocation"
-          name="roomLocation"
-          value={roomData.roomLocation}
-          onChange={handleChange}
+          {...register('roomLocation', {
+            required: 'Lokalizacja jest wymagana',
+            minLength: {
+              value: 3,
+              message: 'Nazwa musi mieć co najmniej 3 znaki',
+            },
+            maxLength: {
+              value: 30,
+              message: 'Nazwa nie może mieć więcej niż 20 znaków',
+            },
+          })}
           placeholder="np. 2 piętro"
         />
+        {errors.roomLocation && (
+          <span className="error">{errors.roomLocation.message}</span>
+        )}
 
         <Button
           variant="contained"
