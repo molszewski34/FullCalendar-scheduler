a66904e276292b37035a0c652b76cab8de13e2f3
molszewski34 <m.olszewski341@gmail.com>
2024-03-06 08:38:50 +0100

    feat: added tooltip feature with react-tooltip lib

--- client/src/Components/modals/RoomSelectionModal.jsx
+++ client/src/Components/modals/RoomSelectionModal.jsx
@@ -5,6 +5,8 @@ import LocalHotelIcon from '@mui/icons-material/LocalHotel';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import '../styles/RoomSelectionModal.css';
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 const RoomSelectionModal = () => {
   const {
     setRoom,
@@ -69,25 +71,55 @@ const RoomSelectionModal = () => {
                   borderColor: room.roomColor,
                   marginBottom: '.25em',
                   padding: '1em 0.3em',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  backgroundColor: ' #f1f5f9',
                 }}
               >
-                <div className="modal-room-selection_info__top">
-                  <span>
-                    <LocalHotelIcon />
-                    {room.roomNumOfGuests}
-                  </span>
-                  <span>
-                    <AttachMoneyIcon />
-                    {room.RoomPriceOfGuest}zł/os
-                  </span>
+                <div className="modal-room-selection_info__wrapper">
+                  <div className="modal-room-selection_info__top">
+                    <span>
+                      <LocalHotelIcon />
+                      {room.roomNumOfGuests}
+                    </span>
+                    <span>
+                      <AttachMoneyIcon />
+                      {room.RoomPriceOfGuest}zł/os
+                    </span>
+                  </div>
+                  {room.roomName}
+                  <div className="modal-room-selection_info__bottom">
+                    <span>
+                      <LocationOnIcon />
+                      {room.roomLocation}
+                    </span>
+                  </div>
                 </div>
-                {room.roomName}
-                <div className="modal-room-selection_info__bottom">
-                  <span>
-                    <LocationOnIcon />
-                    {room.roomLocation}
-                  </span>
+                <div className="modal-room-selection_info-equipment__wrapper">
+                  {room.equipment.map((equipment, index) => (
+                    <>
+                      <span
+                        data-tooltip-id="equipment-tooltip"
+                        key={index}
+                        className="modal-room-selection_info-equipment__wrapper__icon"
+                        data-tooltip-content={equipment.name} // Set tooltip text here
+                        data-tooltip-place="top"
+                        data-tooltip-variant="success"
+                        // data-for={`equipment-tooltip-${index}`}
+                        style={{ fontFamily: 'Material Icons' }}
+                      >
+                        {equipment.icon}
+                      </span>
+                    </>
+                  ))}
+                  {/* Define tooltips */}
                 </div>
+                <Tooltip
+                  // key={index}
+                  id="equipment-tooltip"
+                  // place="top"
+                  // effect="solid"
+                />
               </button>
             );
             return acc;
